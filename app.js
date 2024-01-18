e215a259de85874f1bd17ae36bb42484919af310
4m-4n <154543946+4m-4n@users.noreply.github.com>
2024-01-18 13:22:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,9 +21,10 @@ app.get("/", (req, res) => {
     res.send("niceee");
 });
 app.use(errormidware);
-app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials:true,
-}))
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials:true,
+// }))
+app.use(cors());
 
